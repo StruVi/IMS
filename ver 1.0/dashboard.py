2e59c3ea0054886839e5f376138291f1485ba934
AV <aswinvibushan848@gmail.com>
2025-01-14 13:24:24 +0530

    Title icon with png

--- ver 1.0/dashboard.py
+++ ver 1.0/dashboard.py
@@ -6,6 +6,7 @@ class IMS:
         self.root.title("Inventory Management System | Developed by Aswin Vibushan")
 
         #====title====
+        self.icon_title=PhotoImage(file="images/carts.png")
         title=Label(self.root,text="Inventory Management System",font=("times new roman",40,"bold"),bg="#010c48",fg="white",anchor="w",padx="20",pady="20").place(x=0,y=0,relwidth=1,height=70)
 
 root=Tk()
