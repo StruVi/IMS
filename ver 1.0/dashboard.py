57ce2fe7263dfe50b0b5fe7caff55cce53d5cae2
AV <aswinvibushan848@gmail.com>
2025-01-14 13:18:47 +0530

    Bg and Fg color for title

--- ver 1.0/dashboard.py
+++ ver 1.0/dashboard.py
@@ -6,7 +6,7 @@ class IMS:
         self.root.title("Inventory Management System | Developed by Aswin Vibushan")
 
         #====title====
-        title=Label(self.root,text="Inventory Management System",font=("times new roman",40,"bold")).place(x=0,y=0,relwidth=1,height=70)
+        title=Label(self.root,text="Inventory Management System",font=("times new roman",40,"bold"),bg="#010c48",fg="white").place(x=0,y=0,relwidth=1,height=70)
 
 root=Tk()
 obj=IMS(root)
