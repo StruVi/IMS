2adab340fa0ecc4edbe83fcc19826b7b4dc8a711
AV <aswinvibushan848@gmail.com>
2025-01-15 12:14:49 +0530

    Menu bg

--- ver 1.0/dashboard.py
+++ ver 1.0/dashboard.py
@@ -17,7 +17,7 @@ class IMS:
         self.lbl_clock=Label(self.root,text="Welcome to Inventory Management System\t\t Date: DD-MM-YYYY\t\t Time: HH:MM:SS",font=("times new roman",15),bg="#4d636d",fg="white")
         self.lbl_clock.place(x=0,y=70,relwidth=1,height=30)
         #====Menu=====
-        LeftMenu=Frame(self.root,bd=2,relief=RIDGE)
+        LeftMenu=Frame(self.root,bd=2,relief=RIDGE,bg="white")
         LeftMenu.place(x=0,y=102,width=200,height=700)
 
 
