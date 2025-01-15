65f5be2a886b115691477bbed523e5fe5d6ee358
AV <aswinvibushan848@gmail.com>
2025-01-15 12:10:53 +0530

    Menu frame

--- ver 1.0/dashboard.py
+++ ver 1.0/dashboard.py
@@ -14,8 +14,12 @@ class IMS:
         btm_logout=Button(self.root,text="Logout",font=("times new roman",15,"bold"),bg="yellow",cursor="hand2").place(x=1720,y=10,height=50,width=150)
 
         #====Clock====
-        self.lbl_clock=Label(self.root,text="Welcome to IMS\t\t Date: DD-MM-YYYY\t\t Time: HH:MM:SS",font=("times new roman",15),bg="#4d636d",fg="white")
+        self.lbl_clock=Label(self.root,text="Welcome to Inventory Management System\t\t Date: DD-MM-YYYY\t\t Time: HH:MM:SS",font=("times new roman",15),bg="#4d636d",fg="white")
         self.lbl_clock.place(x=0,y=70,relwidth=1,height=30)
+        #====Menu=====
+        LeftMenu=Frame(self.root,bd=2,relief=RIDGE)
+        LeftMenu.place(x=0,y=102,width=200,height=565)
+
 
 root=Tk()
 obj=IMS(root)
