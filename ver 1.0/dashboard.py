5dd3a8f496ab376d2f0b9380b05c118f51246d89
AV <aswinvibushan848@gmail.com>
2025-01-15 12:25:20 +0530

    Add Menu Icon

--- ver 1.0/dashboard.py
+++ ver 1.0/dashboard.py
@@ -5,6 +5,7 @@ class IMS:
         self.root=root
         self.root.geometry("1920x1080+0+0")
         self.root.title("Inventory Management System | Developed by Aswin Vibushan")
+        self.root.config(bg="white")
 
         #====title====
         self.icon_title=PhotoImage(file="images/carts.png")
@@ -16,7 +17,9 @@ class IMS:
         #====Clock====
         self.lbl_clock=Label(self.root,text="Welcome to Inventory Management System\t\t Date: DD-MM-YYYY\t\t Time: HH:MM:SS",font=("times new roman",15),bg="#4d636d",fg="white")
         self.lbl_clock.place(x=0,y=70,relwidth=1,height=30)
+
         #====Menu=====
+        self.Menu_icon=Image.open("images\IM-menu.png")
         LeftMenu=Frame(self.root,bd=2,relief=RIDGE,bg="white")
         LeftMenu.place(x=0,y=102,width=200,height=700)
 
