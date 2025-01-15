5f3f33c5004b2e62e55cfd5b4d81b4d06551615d
AV <aswinvibushan848@gmail.com>
2025-01-15 21:32:01 +0530

    Menu btns

--- ver 1.0/dashboard.py
+++ ver 1.0/dashboard.py
@@ -12,7 +12,7 @@ class IMS:
         title=Label(self.root,text="Inventory Management System",image=self.icon_title,compound=LEFT,font=("times new roman",40,"bold"),bg="#010c48",fg="white",anchor="w",padx="20").place(x=0,y=0,relwidth=1,height=70)
 
         #====btn_logout====
-        btm_logout=Button(self.root,text="Logout",font=("times new roman",15,"bold"),bg="yellow",cursor="hand2").place(x=1720,y=10,height=50,width=150)
+        btn_logout=Button(self.root,text="Logout",font=("times new roman",15,"bold"),bg="yellow",cursor="hand2").place(x=1720,y=10,height=50,width=150)
 
         #====Clock====
         self.lbl_clock=Label(self.root,text="Welcome to Inventory Management System\t\t Date: DD-MM-YYYY\t\t Time: HH:MM:SS",font=("times new roman",15),bg="#4d636d",fg="white")
@@ -24,10 +24,22 @@ class IMS:
         self.Menu_icon=ImageTk.PhotoImage(self.Menu_icon)
 
         LeftMenu=Frame(self.root,bd=2,relief=RIDGE,bg="white")
-        LeftMenu.place(x=0,y=102,width=200,height=700)
+        LeftMenu.place(x=0,y=102,width=200,height=565)
 
         lbl_MenuIcon=Label(LeftMenu,image=self.Menu_icon)
         lbl_MenuIcon.pack(side=TOP,fill=X)
+
+        self.icon_side=PhotoImage(file="images/side.png")
+
+        lbl_menu=Label(LeftMenu,text="Menu",font=("times new roman",20),bg="#009688").pack(side=TOP,fill=X)
+        btn_employee=Button(LeftMenu,text="Employee",image=self.icon_side,compound=LEFT,padx=5,anchor="w",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_supplier=Button(LeftMenu,text="Supplier",image=self.icon_side,compound=LEFT,padx=5,anchor="w",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_category=Button(LeftMenu,text="Category",image=self.icon_side,compound=LEFT,padx=5,anchor="w",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_product=Button(LeftMenu,text="Products",image=self.icon_side,compound=LEFT,padx=5,anchor="w",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_sales=Button(LeftMenu,text="Sales",image=self.icon_side,compound=LEFT,padx=5,anchor="w",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+        btn_exit=Button(LeftMenu,text="Exit",image=self.icon_side,compound=LEFT,padx=5,anchor="w",font=("times new roman",20,"bold"),bg="white",bd=3,cursor="hand2").pack(side=TOP,fill=X)
+
+
 root=Tk()
 obj=IMS(root)
 root.mainloop()
