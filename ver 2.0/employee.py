5b81e9251dc28d8dcc79db192fd1eb547155fc40
AV <aswinvibushan848@gmail.com>
2025-01-18 12:13:41 +0530

    Main-1

--- ver 2.0/employee.py
+++ ver 2.0/employee.py
@@ -1,8 +1,14 @@
 from tkinter import*
 from PIL import Image,ImageTk #pip install pillow
-class IMS:
+class employeeClass:
     def __init__(self,root):
         self.root=root
         self.root.geometry("1920x1080+0+0")
         self.root.title("Inventory Management System | Developed by 6cyn")
         self.root.config(bg="white")
+
+
+if __name__=="__main__":
+root=Tk()
+obj=employeeClass(root)
+root.mainloop()
