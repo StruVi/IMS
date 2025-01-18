016c6bbd341bdca1ed4fa41aa6df77609bc8cde4
AV <aswinvibushan848@gmail.com>
2025-01-18 12:41:14 +0530

    Search Frame

--- ver 2.0/employee.py
+++ ver 2.0/employee.py
@@ -1,5 +1,6 @@
 from tkinter import*
 from PIL import Image,ImageTk #pip install pillow
+from tkinter import ttk
 class employeeClass:
     def __init__(self,root):
         self.root=root
@@ -7,6 +8,13 @@ class employeeClass:
         self.root.title("Inventory Management System | Developed by 6cyn")
         self.root.config(bg="white")
         self.root.focus_force()
+
+        #====SearchFrame====
+        SearchFrame=LabelFrame(self.root,text="Search Employee",font=("goudy old style",12,"bold"),bd=2,relief=RIDGE,bg="white")
+        SearchFrame.place(x=250,y=20,width=600,height=70)
+
+        
+
 
 
 
