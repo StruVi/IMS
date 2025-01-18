d9ed5cba90330429439c75c0b8daf843f658c7a5
AV <aswinvibushan848@gmail.com>
2025-01-18 12:14:56 +0530

    Geometry Resolution

--- ver 2.0/employee.py
+++ ver 2.0/employee.py
@@ -3,7 +3,7 @@ from PIL import Image,ImageTk #pip install pillow
 class employeeClass:
     def __init__(self,root):
         self.root=root
-        self.root.geometry("1920x1080+0+0")
+        self.root.geometry("1100x500+220+130")
         self.root.title("Inventory Management System | Developed by 6cyn")
         self.root.config(bg="white")
 
