cfd14698a5d18d4343e6d88b9be8ba2985d31c73
Little Boi <aswinvibushan848@gmail.com>
2025-01-29 01:11:14 +0530

    Crt curs command

--- ver 2.0/create_db.py
+++ ver 2.0/create_db.py
@@ -2,19 +2,7 @@ import sqlite3
 def create_db():
     con=sqlite3.connect(database=r'ims.db')
     cur=con.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS employee(
-        Emp-ID INTEGER PRIMARY KEY AUTOINCREMENT,
-        Name TEXT NOT NULL,
-        Email TEXT NOT NULL,
-        Gender TEXT NOT NULL,
-        Contact TEXT NOT NULL,
-        DOB TEXT NOT NULL,
-        DOJ TEXT NOT NULL,
-        Password TEXT NOT NULL,
-        UserType TEXT NOT NULL,
-        Address TEXT NOT NULL,
-        Salary TEXT NOT NULL
-    )''')
+    cur.execute("CREATE TABLE IF NOT EXISTS employee(eid INTEGER PRIMARY KEY AUTOINCREMENT,Name text,Email text,Gender text,Contact text,DOB text,DOJ text,Password text,UserType text,Address text,Salary text)")
     con.commit()
               
 create_db()
