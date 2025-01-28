d177bf7f0c860167ecd52b3430f8c94d7630230f
Little Boi <aswinvibushan848@gmail.com>
2025-01-29 01:03:22 +0530

    add cursor and cursor execution instructions

--- ver 2.0/create_db.py
+++ ver 2.0/create_db.py
@@ -1,5 +1,20 @@
 import sqlite3
 def create_db():
     con=sqlite3.connect(database=r'ims.db')
+    cur=con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS employee(
+        Emp-ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Name TEXT NOT NULL,
+        Email TEXT NOT NULL,
+        Gender TEXT NOT NULL,
+        Contact TEXT NOT NULL,
+        DOB TEXT NOT NULL,
+        DOJ TEXT NOT NULL,
+        Password TEXT NOT NULL,
+        UserType TEXT NOT NULL,
+        Address TEXT NOT NULL,
+        Salary TEXT NOT NULL
+    )''')
+    con.commit()
               
 create_db()
